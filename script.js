5b157778f85cf0a1e2f4cfe021f7e812955b0e9a
kim <Kimvz19@users.noreply.github.com>
2025-02-06 14:43:46 +0100

    adding informatie

--- script.js
+++
@@ -1,5 +0,0 @@
-// Laadt de Spline Viewer module dynamisch in
-const script = document.createElement("script");
-script.type = "module";
-script.src = "https://unpkg.com/@splinetool/viewer@1.9.62/build/spline-viewer.js";
-document.body.appendChild(script);
