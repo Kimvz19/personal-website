4fa1c65e79235b91141e9a1078583bbfcfafb33a
kim <Kimvz19@users.noreply.github.com>
2025-02-11 10:26:42 +0100

    updating the working

--- script.js
+++ script.js
@@ -21,21 +21,41 @@ fetch(myURL)
 
 
 
-
-
-function addNumber(num) {
-    document.getElementById("codeInput").value += num;
+//Functies voor het invoerveld en mijn producten.
+// Functie om het nummer toe te voegen aan de input
+function addNumber(number) {
+    let codeInput = document.getElementById('codeInput');
+    codeInput.value += number;
 }
 
+// Functie om de input te wissen
 function clearInput() {
-    document.getElementById("codeInput").value = "";
+    document.getElementById('codeInput').value = '';
 }
 
+// Functie om de productinformatie in te vullen en zichtbaar te maken
 function submitCode() {
-    let code = document.getElementById("codeInput").value;
-    if (code.length > 0) {
-        alert("Code ingevoerd: " + code);
+    let code = document.getElementById('codeInput').value;
+
+    // Verberg alle dialoogvensters eerst
+    let allDialogs = document.querySelectorAll('dialog');
+    allDialogs.forEach(function(dialog) {
+        dialog.close();
+    });
+
+    // Zoek de specifieke dialoog voor de ingevoerde code en toon deze
+    let dialog = document.getElementById('product-info-' + code);
+    if (dialog) {
+        dialog.showModal(); // Toont het dialoog
     } else {
-        alert("Voer eerst een code in!");
+        alert("Geen informatie beschikbaar voor deze productcode.");
     }
+
+    // Leeg het invoerveld na het tonen van de informatie
+    document.getElementById('codeInput').value = '';
+}
+
+// Functie om een dialoog te sluiten
+function closeDialog(dialogId) {
+    document.getElementById(dialogId).close();
 }
